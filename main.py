6b824baae9e110ce59262a05371d7dd787251516
Julian Palmerio <julianpalmerio@hotmail.com>
2025-03-15 14:18:57 -0300

    test: add pay with balance tests

--- main.py
+++ main.py
@@ -184,6 +184,28 @@ class TestUser(unittest.TestCase):
         with self.assertRaises(CreditCardException):
             user.add_credit_card("4242424242424242")
 
+    def test_pay_with_balance_success(self):
+        bobby = User("Bobby")
+        carol = User("Carol")
+        bobby.add_to_balance(5.00)
+        payment = bobby.pay_with_balance(carol, 5.00, "Coffee")
+        self.assertEqual(bobby.balance, 0.00)
+        self.assertEqual(carol.balance, 5.00)
+        self.assertEqual(payment.amount, 5.00)
+        self.assertEqual(payment.actor, bobby)
+        self.assertEqual(payment.target, carol)
+        self.assertEqual(payment.note, "Coffee")
+
+    def test_pay_with_balance_failure(self):
+        bobby = User("Bobby")
+        carol = User("Carol")
+        with self.assertRaises(PaymentException):
+            bobby.pay_with_balance(carol, 0.00, "Coffee")
+        with self.assertRaises(PaymentException):
+            bobby.pay_with_balance(carol, 5.00, "Coffee")
+        with self.assertRaises(PaymentException):
+            bobby.pay_with_balance(bobby, 5.00, "Coffee")
+
     def test_pay_with_card_success(self):
         bobby = User("Bobby")
         carol = User("Carol")
